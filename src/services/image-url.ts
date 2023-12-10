349b37979dcfc5c204cb55af39feb23923a5746c
Salahuddin <salahuddinshaikh16@gmail.com>
2023-12-10 11:08:08 +0530

    Get optimised images

---
+++ src/services/image-url.ts
@@ -0,0 +1,9 @@
+import React from "react";
+
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  const startIndex = url.indexOf(target) + target.length;
+  return url.slice(0, startIndex) + "crop/600/400/" + url.slice(startIndex);
+};
+
+export default getCroppedImageUrl;
