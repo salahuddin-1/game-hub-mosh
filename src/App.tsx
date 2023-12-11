215a05c364f99ac2abeab6fa93a38ba08608ab98
Salahuddin <salahuddinshaikh16@gmail.com>
2023-12-11 10:40:43 +0530

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
 
       {/* Show this component only if the width is >= lg (wider than 1024px) */}
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside">
+          <GenreList />
+        </GridItem>
       </Show>
 
       <GridItem area="main">
