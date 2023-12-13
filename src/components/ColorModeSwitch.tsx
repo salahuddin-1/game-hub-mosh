67d58d7fa62309264503e5d4452943873fdfa570
Salahuddin <salahuddinshaikh16@gmail.com>
2023-12-13 13:34:55 +0530

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -12,7 +12,7 @@ const ColorModeSwitch = () => {
         onChange={() => toggleColorMode()}
         colorScheme="green"
       ></Switch>
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
