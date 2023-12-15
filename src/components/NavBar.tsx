faa6940eb98c69bf46fbe3fad5305b7045d86f80
Salahuddin <salahuddinshaikh16@gmail.com>
2023-12-15 17:30:06 +0530

    Nav bar icon change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
@@ -10,8 +10,15 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="20px">
-      <Image src={logo} boxSize="50px"></Image>
-      <SearchInput onSearch={onSearch} />
+      {/* <Image src={logo} boxSize="50px"></Image> */}
+      <Text letterSpacing={3} fontSize="large" as="b" whiteSpace="nowrap">
+        R A W G
+      </Text>
+
+      <Box paddingX="30px" width="100%">
+        <SearchInput onSearch={onSearch} />
+      </Box>
+
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
